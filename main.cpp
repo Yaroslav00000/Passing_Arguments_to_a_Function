01b3cab6564cfba9ad38de86dd83e6a8234b94ba
Yaremchuk_Yaroslav <yaroslavyaremchuk228@gmail.com>
2024-11-05 17:17:18 +0200

    task complited

--- main.cpp
+++ main.cpp
@@ -1,5 +1,43 @@
 #include <iostream>
-#include <string>
+#include <windows.h>
 
 using namespace std;
 
+int binarySearch(int arr[], int size, int key) {
+    int left = 0;
+    int right = size - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (arr[mid] == key) {
+            return mid;
+        }
+        if (arr[mid] < key) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+
+    return 0;
+};
+int main(){
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int arr[] = {10,20,40,40,50};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int key = 30;
+
+    int result = binarySearch(arr, size, key);
+
+    if (result != 0) {
+        cout << "Елемент знайдено на індексі: " << result << endl;
+    }
+    else {
+        cout << "Елемент не знайдено" << endl;
+    }
+
+    return 0;
+
+}
